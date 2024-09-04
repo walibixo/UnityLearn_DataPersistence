14d4d937432f9bb34551c45027a3c77fcd1d1169
walibixo <chatot.antoine@gmail.com>
2024-09-05 00:02:27 +0200

    Initial project commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float Speed = 2.0f;
+    public float MaxMovement = 2.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+
+        Vector3 pos = transform.position;
+        pos.x += input * Speed * Time.deltaTime;
+
+        if (pos.x > MaxMovement)
+            pos.x = MaxMovement;
+        else if (pos.x < -MaxMovement)
+            pos.x = -MaxMovement;
+
+        transform.position = pos;
+    }
+}
